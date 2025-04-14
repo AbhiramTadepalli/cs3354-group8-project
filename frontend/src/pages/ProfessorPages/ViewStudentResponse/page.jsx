b993d375defcccfacdf0b53514ff717d0087e4a8
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-04-14 15:29:57 -0500

    delete the old ViewStudentResponse Page

--- frontend/src/pages/ProfessorPages/ViewStudentResponse/page.jsx
+++
@@ -1,9 +0,0 @@
-//Nishant Bhagat
-import React from 'react';
-import NavBarProfessor from '../../../Components/NavBarProfessor'; 
-
-const ViewStudentResponse = () => {
-  
-}
-
-export default ViewStudentResponse
