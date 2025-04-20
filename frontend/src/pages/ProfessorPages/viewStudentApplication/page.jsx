166e9c3c8361b9711a69a23bdc119dd850701bec
NidhiMajoju <majoju.nidhi@gmail.com>
2025-04-19 23:16:42 -0500

    fixed navbar

--- frontend/src/pages/ProfessorPages/viewStudentApplication/page.jsx
+++ frontend/src/pages/ProfessorPages/viewStudentApplication/page.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import NavBar from '../../../Components/NavBarStudent';
+import NavBar from '../../../Components/NavBarProfessor';
 import { useParams } from 'react-router-dom';
 
 // Status styling
